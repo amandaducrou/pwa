32aa0ff6e03d6cc4704516eff1fa7daa3fc80aef
Amanda Ducrou <aducrou@expedia.com>
2018-05-03 16:32:28 +1000

    message and push examples

--- main.js
+++ main.js
@@ -1,21 +1,28 @@
 
 // Progressive Enhancement (SW supported)
-// if ('serviceWorker' in navigator) {
+// if ("serviceWorker" in navigator) {
 if (navigator.serviceWorker) {
 
   // Register the SW
-  navigator.serviceWorker.register('/sw.js').then(function(registration){
+  navigator.serviceWorker.register("/sw.js").then(function(registration) {
 
-    console.log('SW Registered');
+    console.log("SW Registered");
 
+    if(registration.active){
+        registration.active.postMessage("Hello from main.js");
+    }
   }).catch(console.log);
 
+  // print messages sent to this client
+  navigator.serviceWorker.addEventListener("message", (e) => {
+    console.log(e.data);
+  });
 }
 
 // Get camera feed
-fetch('camera_feed.html')
+fetch("camera_feed.html")
   .then((res) => {
   return res.text();
 }).then((html) => {
-  document.getElementById('camera').innerHTML = html;
+  document.getElementById("camera").innerHTML = html;
 });
--- sw.js
+++ sw.js
@@ -1,15 +1,34 @@
 
 // Service Worker
-self.addEventListener('fetch', (e) => {
+self.addEventListener("fetch", (e) => {
 
-  if (e.request.url.endsWith('/camera_feed.html')) {
+  if (e.request.url.endsWith("/camera_feed.html")) {
 
     e.respondWith(
       fetch(e.request)
         .then((res) => {
           if( res.ok ) return res;
-          return new Response('Camera feed currently not available.')
+          return new Response("Camera feed currently not available.")
         })
     )
   }
+});
+
+self.addEventListener("message", (e) => {
+
+    // log message sent to service worker
+    console.log(e.data);
+
+    // reply to posting client
+    self.clients.matchAll().then((clients) => {
+        clients.forEach((client) => {
+            if(e.source.id === client.id) {
+                client.postMessage("Private Hello from Service Worker");
+            }
+        });
+    });
+});
+
+self.addEventListener("push", (e) => {
+    console.log("Push Received", e.message);
 });
